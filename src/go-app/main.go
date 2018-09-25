1c7582177dff65618610e68f832cb5f56c4c9d90
joonlee122 <joon@xponentialworks.com>
2018-09-25 16:28:04 -0700

    Created Main.go

---
+++ src/go-app/main.go
@@ -0,0 +1,21 @@
+package main
+
+import(
+	"fmt"
+	"net/http"
+
+	"google.golang.org/appengine" // Required external App Engine Library
+)
+func indexHandler(w http.ResponseWriter, r *http.Request){
+	if r.URL.Path != "/" {
+		http.Redirect(w, r, "/", http.StatusFound)
+		return
+	}
+
+	fmt.Fprintln(w, "Hello, World!")
+}
+
+func main(){
+	http.HandleFunc("/", indexHandler)
+	appengine.Main()
+}
