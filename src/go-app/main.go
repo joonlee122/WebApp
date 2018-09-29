cc279d805ab7100a37fba35a38f8881891d13260
joonlee122 <joon@xponentialworks.com>
2018-09-28 18:01:30 -0700

    Handling HTML form data

--- src/go-app/main.go
+++ src/go-app/main.go
@@ -2,7 +2,8 @@ package main
 
 import(
 	"net/http"
-
+	"fmt"
+	"html/template"
 	"google.golang.org/appengine" // Required external App Engine Library
 )
 func indexHandler(w http.ResponseWriter, r *http.Request){
@@ -10,6 +11,40 @@ func indexHandler(w http.ResponseWriter, r *http.Request){
 		http.Redirect(w, r, "/", http.StatusFound)
 		return
 	}
+
+	params := templateParams{}
+
+	if r.Method == "GET" {
+		indexTemplate.Execute(w, params)
+		return
+	}
+
+	//It's a POST request, so handle the form submission
+
+	name := r.FormValue("name")
+	params.Name = name
+	if name == "" {
+		name = "Anonymous Gopher"
+	}
+
+	if r.FormValue("message") == "" {
+		w.WriteHeader(http.StatusBadRequest)
+
+		params.Notice = "No message provided"
+		indexTemplate.Execute(w, params)
+		return
+	}
+
+	params.Notice = fmt.Sprintf("Thank you for your submission, %s!", name)
+}
+
+var (
+	indexTemplate = template.Must(template.ParseFiles("index.html"))
+)
+
+type templateParams struct {
+	Notice string
+	Name string
 }
 
 func main(){
