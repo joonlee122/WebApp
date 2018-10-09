0a6b518d82536c558edb7c3f2d391fdc5b057d83
joonlee122 <43582551+joonlee122@users.noreply.github.com>
2018-10-08 17:32:13 -0700

    Trying watching a sample directory in local machine

--- src/go-app/main.go
+++ src/go-app/main.go
@@ -1,53 +1,42 @@
 package main
 
 import(
-	"net/http"
 	"fmt"
-	"html/template"
 	"google.golang.org/appengine" // Required external App Engine Library
+	"github.com/go-fsnotifiy/fsnotify"
+	
 )
-func indexHandler(w http.ResponseWriter, r *http.Request){
-	if r.URL.Path != "/" {
-		http.Redirect(w, r, "/", http.StatusFound)
-		return
-	}
-
-	params := templateParams{}
-
-	if r.Method == "GET" {
-		indexTemplate.Execute(w, params)
-		return
-	}
-
-	//It's a POST request, so handle the form submission
-
-	name := r.FormValue("name")
-	params.Name = name
-	if name == "" {
-		name = "Anonymous Gopher"
-	}
-
-	if r.FormValue("message") == "" {
-		w.WriteHeader(http.StatusBadRequest)
-
-		params.Notice = "No message provided"
-		indexTemplate.Execute(w, params)
-		return
-	}
-
-	params.Notice = fmt.Sprintf("Thank you for your submission, %s!", name)
-}
-
-var (
-	indexTemplate = template.Must(template.ParseFiles("index.html"))
-)
-
-type templateParams struct {
-	Notice string
-	Name string
-}
 
 func main(){
-	http.HandleFunc("/", indexHandler)
-	appengine.Main()
+	// creates a new file watcher
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		fmt.Println("ERROR", err)
+	}
+	defer watcher.Close()
+
+	//
+	done := make(chan bool)
+
+	//
+	go func() {
+		for {
+			select {
+			// watch for events
+			case event := <-watcher.Events:
+				fmt.Printf("EVENT! %#v\n", event)
+
+				// watch for errors
+			case err := <-watcher.Errors:
+				fmt.Println("ERROR", err)
+			}
+		}
+	}()
+
+	// out of the box fsnotify can watch a single file, or a single directory
+	if err := watcher.Add("/Users/ttoya/Desktop/XponentialWorks"); err != nil {
+		fmt.Println("ERROR", err)
+	}
+
+	<-done
 }
